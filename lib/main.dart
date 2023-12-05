cfeecc8f2c070b389f1f168e8e9d9747157c10e2
Kunal <kunalprajapat65@gmail.com>
2023-12-06 00:17:57 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:kiddo_rights/home_page.dart';
 
-void main(){
+void main() {
   runApp(Kiddo());
-  }
-
+}
 
 class Kiddo extends StatelessWidget {
   const Kiddo({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    String naam = "By Kunal";
+    
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to Kiddo Rights"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
